98059724e661fa13af0fe32f29854ade0a5ace51
Churro <66274903+ChurroC@users.noreply.github.com>
2024-11-11 17:57:26 +0000

    Connected Youtube API and can swap youtube video perfectly

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,7 +1,6 @@
 import { useRef } from "react";
 
-import {  useQuery
-} from '@tanstack/react-query'
+import { useQuery } from "@tanstack/react-query";
 
 export function Search({
     setVideoId
@@ -9,24 +8,32 @@ export function Search({
     setVideoId: React.Dispatch<React.SetStateAction<string | null>>;
 }) {
     const { isPending, error, data } = useQuery({
-      queryKey: ['repoData'],
-      queryFn: () =>
-        fetch('https://www.googleapis.com/youtube/v3/search').then((res) =>
-          res.json(),
-        ),
-    })
-  
-    if (isPending) return 'Loading...'
-  
-    if (error) return 'An error has occurred: ' + error.message
-  
+        queryKey: ["repoData"],
+        queryFn: () =>
+            fetch(
+                "https://www.googleapis.com/youtube/v3/search?" +
+                    new URLSearchParams({
+                      part: "snippet",
+                      q: "chromakopia",
+                      maxResults: "25",
+                      key: "AIzaSyAWzPr2cdubXGfUIYMN8-g5HyMDgq9wS5U",
+                      type: "video"
+                    }),
+            ).then(res => res.json())
+    });
+
+    if (isPending) return "Loading...";
+
+    if (error) return "An error has occurred: " + error.message;
+
     return (
-      <div>
-        <h1>{data.name}</h1>
-        <p>{data.description}</p>
-        <strong>👀 {data.subscribers_count}</strong>{' '}
-        <strong>✨ {data.stargazers_count}</strong>{' '}
-        <strong>🍴 {data.forks_count}</strong>
-      </div>
-    )
+        <div>
+            <h1>{data.name}</h1>
+            <p>{data.description}</p>
+            {data.items.map((item) => {
+              const id = item.id.videoId
+              return <button key={id} onClick={() => setVideoId(id)}>honjk</button>
+            })}
+        </div>
+    );
 }
