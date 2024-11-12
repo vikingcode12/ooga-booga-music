c309ca26b9aea466a214ebc6e9ac7eab4434997c
Churro <66274903+ChurroC@users.noreply.github.com>
2024-11-12 18:31:18 +0000

    Got search working

--- src/Search.tsx
+++ src/Search.tsx
@@ -4,21 +4,24 @@ import { useQuery } from "@tanstack/react-query";
 
 import type { youtubeAPI } from "./types";
 
+import { Fragment } from "react";
+
 export function Search({
     setVideoId
 }: {
     setVideoId: React.Dispatch<React.SetStateAction<string | null>>;
 }) {
     const [videoSearch, setVideoSearch] = useState("");
+    console.log(videoSearch)
 
     const { isPending, error, data } = useQuery<youtubeAPI>({
-        queryKey: ["videoSearch"],
+        queryKey: ["videoSearch", videoSearch],
         queryFn: () =>
             fetch(
                 "https://www.googleapis.com/youtube/v3/search?" +
                     new URLSearchParams({
                         part: "snippet",
-                        q: "chromakopia",
+                        q: videoSearch,
                         maxResults: "25",
                         key: "AIzaSyAWzPr2cdubXGfUIYMN8-g5HyMDgq9wS5U",
                         type: "video"
@@ -26,20 +29,31 @@ export function Search({
             ).then(res => res.json()),
     });
 
-    if (isPending) return "Loading...";
-
     if (error) return "An error has occurred: " + error.message;
 
-    return data.items.map(item => (
+    return (
         <>
-            <h1>{item.snippet.title}</h1>
-            <p>{item.snippet.description}</p>
-            <button
-                key={item.id.videoId}
-                onClick={() => setVideoId(item.id.videoId)}
-            >
-                honjk
-            </button>
+            <input
+                type="text"
+                value={videoSearch}
+                onChange={event => {
+                    setVideoSearch(event.target.value);
+                }}
+            />
+            {isPending
+                ? "Loading..."
+                : data.items.map(item => (
+                      <Fragment key={item.id.videoId}>
+                          <h1>{item.snippet.title}</h1>
+                          <p>{item.snippet.description}</p>
+                          <button
+                              key={item.id.videoId}
+                              onClick={() => setVideoId(item.id.videoId)}
+                          >
+                              honjk
+                          </button>
+                      </Fragment>
+                  ))}
         </>
-    ))
+    );
 }
