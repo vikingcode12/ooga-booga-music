b53e107dc94aa9893f73127cb0ccfa6d892b3735
ChurroC <66274903+ChurroC@users.noreply.github.com>
2025-03-12 16:15:54 +0000

    added rooms through url param

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Search } from "./Search";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query";
 import usePartySocket from "partysocket/react";
 
 const queryClient = new QueryClient();
@@ -10,10 +10,12 @@ export default function App() {
     const [videoId, setVideoId] = useState<string | null>("0");
     const [message, setMessages] = useState("");
 
+    console.log(new URL(document.location.toString()).searchParams.get("room") ?? "main")
+
     const ws = usePartySocket({
         // usePartySocket takes the same arguments as PartySocket.
-        host: import.meta.env.VITE_SERVER_URL, // or localhost:1999 in dev
-        room: "c-dog",
+        host: "https://humble-pancake-g655xpppgx92749-1999.app.github.dev/", // or localhost:1999 in dev
+        room: new URL(document.location.toString()).searchParams.get("room") ?? "main",
         // in addition, you can provide socket lifecycle event handlers
         // (equivalent to using ws.addEventListener in an effect hook)
         onOpen() {
