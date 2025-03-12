755b384e8b945b55b035df04f860b02e3af85bcb
ChurroC <66274903+ChurroC@users.noreply.github.com>
2025-03-12 17:25:20 +0000

    playing syncs but one can override the other

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { Search } from "./Search";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import usePartySocket from "partysocket/react";
@@ -13,8 +13,6 @@ export default function App() {
     const [player, setPlayer] = useState<YouTubePlayer>();
     const [videoId, setVideoId] = useState<string>("u9n7Cw-4_HQ"); // Turn into array of future videos
     const [message, setMessages] = useState("");
-
-    const autoplay = 1; // 1 if it's autoplay else it's 0
 
     const ws = usePartySocket({
         host: "https://humble-pancake-g655xpppgx92749-1999.app.github.dev/", // or localhost:1999 in dev
@@ -32,13 +30,18 @@ export default function App() {
                 return setVideoId(data.videoId)
             }
 
-            if (data.id = "videoInfo") {
-                const playing = data.playing === 1 ? true: false
+            if (data.id === "videoInfo") {
+                const playing = data.playing
                 const time = data.time
 
-                
-
                 player.seekTo(time, true)
+
+                if (playing) {
+                    player.playVideo();
+                } else {
+                    player.pauseVideo();
+                }
+
             }
         },
         onClose() {
@@ -57,19 +60,15 @@ export default function App() {
                 <YouTube videoId={videoId} opts={{
                     height: "360",
                     width: "640",
-                    playerVars: {
-                      // https://developers.google.com/youtube/player_parameters
-                      autoplay: 1,
-                    },
                 }} onReady={(e) => {
+                    ws.send(
+                        JSON.stringify({
+                            id: "videoInfo",
+                            playing: false,
+                            time: 0,
+                        })
+                    );
                     setPlayer(e.target)
-                    // ws.send(
-                    //     JSON.stringify({
-                    //         id: "videoInfo",
-                    //         playing: autoplay === 1 ? true : false,
-                    //         time: 0,
-                    //     })
-                    // );
                 }} onPause={(e) => {
                     ws.send(
                         JSON.stringify({
